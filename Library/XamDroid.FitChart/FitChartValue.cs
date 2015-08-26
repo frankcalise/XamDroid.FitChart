f198ea1b033fc5f8dfe2f6ebd7c0b4d70a9ceacd
Frank Calise <fcalise@gmail.com>
2015-08-26 17:16:11 -0400

    update the paint color in Paint setter

--- Library/XamDroid.FitChart/FitChartValue.cs
+++ Library/XamDroid.FitChart/FitChartValue.cs
@@ -7,15 +7,27 @@ namespace com.frankcalise.widgets
 	{
 		public float Value { get; private set; }
 
-		public int Color { get; private set; }
+		public Color Color { get; private set; }
 
-		public Paint Paint { get; set; }
+		private Paint _paint;
+		public Paint Paint
+		{
+			get
+			{
+				return _paint;
+			} 
+			set
+			{
+				_paint = value;
+				_paint.Color = Color;
+			}
+		}
 
 		public float StartAngle { get; set; }
 
 		public float SweepAngle { get; set; }
 
-		public FitChartValue(float value, int color)
+		public FitChartValue(float value, Color color)
 		{
 			Value = value;
 			Color = color;
